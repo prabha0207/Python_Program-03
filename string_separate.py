31b447f06c35f44caef030ba860cfaee8114aaa9
Swarnaprabha M <swarnaprabhamaha@gmail.com>
2019-06-07 17:31:15 +0100

    Create string_separate.py

---
+++ string_separate.py
@@ -0,0 +1,13 @@
+# test case
+  #Input : sanjay 
+  #Output; ['s','a', 'n', 'j', 'a', 'y,]
+#first input
+string=input()
+x=[char for char in string] #seperate in charater in input
+print(x)
+
+
+#using map()
+#number=list(map(str,input()))
+
+    
